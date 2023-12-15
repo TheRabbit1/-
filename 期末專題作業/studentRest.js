d63db2339ec41b8c753fa0da00fabb8be8a42d1e
TheRabbit1 <146634928+TheRabbit1@users.noreply.github.com>
2023-12-16 01:29:22 +0800

    Add files via upload

---
+++ 期末專題作業/studentRest.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$("#b1").click(function() {
+		/* Act on the event */
+		$('.article1').toggleClass('open');
+	});
+});
+
+$(document).ready(function() {
+	$("#b2").click(function() {
+		/* Act on the event */
+		$('.article2').toggleClass('open');
+	});
+});
+
+$(document).ready(function() {
+	$("#b3").click(function() {
+		/* Act on the event */
+		$('.article3').toggleClass('open');
+	});
+});
+
+$(document).ready(function() {
+	$("#b4").click(function() {
+		/* Act on the event */
+		$('.article4').toggleClass('open');
+	});
+});
